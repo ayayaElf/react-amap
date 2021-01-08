d77402c72a7a89ea2b8ac6d26baed029c50dd25b
ayayaElf <1550252538@qq.com>
2021-01-08 16:58:55 +0800

    添加Sass插件，增加weaterCom组件

--- src/components/weaterCom/weaterCom.js
+++ src/components/weaterCom/weaterCom.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
 import public_fun from '../../Common/public_fun';
-import {getLive} from '../../model/AMap';
+import {getLive, getForecast} from '../../model/AMap';
+import './weaterCom.scss';
 
-function WeaterCom(props) {
-    if (public_fun.isEmptyObject(props.weaterInfo)) return <div></div>
-    return <div>{props.weaterInfo.temperature}摄氏度</div>
+function WeatherLiveCom(props) {
+    let element = (
+        <>
+            <div>天气：{props.weatherInfo.weather}</div>
+            <div>气温：{props.weatherInfo.temperature} ℃</div>
+            <div>风向：{props.weatherInfo.windDirection}</div>
+            <div>风力：{props.weatherInfo.windPower} 级</div>
+            <div>空气湿度：{props.weatherInfo.humidity}</div>
+        </>
+    )
+    if (public_fun.isEmptyObject(props.weatherInfo)) return <div>请输入要查询的城市</div>
+    return element
+}
+
+function WeatherForecastCom(props) {
+    if (!public_fun.isEmptyObject(props.weatherInfo)) {
+        const data = props.weatherInfo.forecasts;
+        let element = (
+            <>
+                <div>未来{data.length}天预报</div>
+            </>
+        );
+        return element
+    }
+    return <></>
 }
 
 class WeaterInputCom extends Component {
@@ -12,10 +35,24 @@ class WeaterInputCom extends Component {
         super(props);
         this.state = {
             addr: '',
-            weaterInfo: {},
+            weatherLiveInfo: {},
+            weatherForecastInfo: {},
         }
     }
 
+    getWeatherLiveInfo = async () => {
+        let data = await getLive(this.state.addr);
+        this.setState({
+            weatherLiveInfo: data,
+        })
+    }
+
+    getWeatherForecastInfo = async () => {
+        let data = await getForecast(this.state.addr);
+        this.setState({
+            weatherForecastInfo: data,
+        })
+    }
 
     handleChange = (event) => {
         this.setState({
@@ -23,24 +60,27 @@ class WeaterInputCom extends Component {
         })
     }
 
-    handleClick = () => {
-        getLive(this.state.addr).then(data => {
-            console.log(data);
-            this.setState({
-                weaterInfo: data,
-            })
-        })
+    hanldeClick = () => {
+        this.getWeatherLiveInfo();
+        this.getWeatherForecastInfo();
+    }
+
+    handleKeyDown = (event) => {
+        if (event.keyCode === 13) {
+            this.getWeatherLiveInfo();
+            this.getWeatherForecastInfo();
+        }
     }
 
     render() {
         return (
-            <div>
-                <div>
-                    请输入要查询的城市
-                    <input value={this.state.addr} onChange={this.handleChange}/>
-                    <input type="submit" value="查询" onClick={this.handleClick}/>
+            <div className="weaterBox">
+                <div className="searchBox">
+                    <input placeholder="请输入要查询的城市" value={this.state.addr} onChange={this.handleChange} onKeyDown={this.handleKeyDown}/>
+                    <input type="submit" value="查询" onClick={this.hanldeClick}/>
                 </div>
-                <WeaterCom weaterInfo={this.state.weaterInfo}/>
+                <WeatherLiveCom weatherInfo={this.state.weatherLiveInfo}/>
+                <WeatherForecastCom weatherInfo={this.state.weatherForecastInfo}/>
             </div>
         )
     }
